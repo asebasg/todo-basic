7265d4da0020531dc4401ecd6fead201880e8820
Sebastián Ospina <asebasg07@gmail.com>
2025-07-21 14:32:56 -0500

    Se mejoro el diseño y el estilo con navegación y contenido centrados

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
+    <div className="content">
       <h1>Bienvenido a To-Do App</h1>
       <p>Esta es una app simple pero funcional, ¡vamos a probarla!</p>
     </div>
